7b33a2e7ba37776e691819dc5bd22e47ac6253df
Your Name <you@example.com>
2013-06-30 14:42:43 +0800

    项目初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riscuz::Application.config.secret_token = 'a06e269d4425d38f84477ce39499e1bd90739d216b91e3d22a7675908c371826eeac1d25b7c357117abfca09d543f8a16462e50ef299cba9680db5517cf1ebe6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Riscuz::Application.config.session_store :cookie_store, key: '_riscuz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Riscuz::Application.config.session_store :active_record_store
